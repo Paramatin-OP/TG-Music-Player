c0d7c353ce541095325849f6bc60a9b5c2b5aa34
Aditya <79218915+Paramatin-OP@users.noreply.github.com>
2021-08-20 11:51:47 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Music-Player
+This is Music Player Repo You can play High Quality music with this repo
---
+++ start.sh
@@ -0,0 +1,5 @@
+echo "Cloning Repo...."
+git clone https://github.com/Paramatin-op/TG-Music-Player.git /TG-Music
+cd /TG-Music
+echo "Starting Bot...."
+python3 main.py
